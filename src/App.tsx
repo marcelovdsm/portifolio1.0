89aa8008809a8b686393d6f36ff514b5a13517f3
Marcelo Victor <marcelovictorsm077@gmail.com>
2023-07-31 18:38:42 -0300

    theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
-function App() {}
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Test</h1>
+    </ThemeProvider>
+  )
+}
 
 export default App
