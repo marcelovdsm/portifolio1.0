14bf15fe5e67796b4c84fb49b73e9a65a7eef71a
Marcelo Victor <marcelovictorsm077@gmail.com>
2023-08-01 15:01:59 -0300

    global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>Test</h1>
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
