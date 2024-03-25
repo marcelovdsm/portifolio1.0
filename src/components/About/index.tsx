ac93ff8025bfa1e0038ac28786fcb289d82cb2da
Marcelo Victor <95694179+marcelovdsm@users.noreply.github.com>
2024-03-25 14:37:26 +0000

    about box

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,19 +1,41 @@
 import photo from '../../assets/marcelo-photo.png'
-import { AboutDivComponent, AboutText } from './styles'
+import {
+  AboutContainer,
+  AboutDivComponent,
+  AboutText,
+  TechIcons,
+} from './styles'
+import { IconCss3 } from '../IconComponents/CssIcon'
+import { IconHtml5 } from '../IconComponents/HtmlIcon'
+import { IconTypescript } from '../IconComponents/TypescriptIcon'
+import { IconReact } from '../IconComponents/ReactIcon'
 
 export const About = () => {
   return (
-    <AboutDivComponent>
-      <img src={photo} alt="Minha foto de perfil" />
-      <AboutText>
-        <h2>Sobre mim</h2>
-        <p id="about">
-          Sou um estudante de análise e desenvolvimento de sistemas e deselvolvo
-          utilizando React, Typescript e Styled Components mas tenho muita
-          disposição para aprender novas linguagens e procuro aperfeiçoar cada
-          vez mais os meus conhecimentos.
-        </p>
-      </AboutText>
-    </AboutDivComponent>
+    <AboutContainer>
+      <AboutDivComponent>
+        <img src={photo} alt="Minha foto de perfil" />
+        <AboutText>
+          <h1>Sobre mim</h1>
+          <p id="about">
+            Eu sou um desenvolvedor front-end junior apaixonado por criar
+            experiências incríveis na web. Apesar de não ter experiência formal,
+            tenho habilidades sólidas em HTML, CSS e TypeScript, e trabalho com
+            Styled Components e React.js. Tenho trabalhado em alguns projetos
+            pessoais e freelances, buscando desafios para me aprimorar. Sou bom
+            em resolver problemas e estou sempre buscando aprender mais.
+            Adoraria participar de projetos interessantes e continuar crescendo
+            como desenvolvedor. Se quiser bater um papo, meu e-mail é
+            marcelovictorsm077@gmail.com.
+          </p>
+        </AboutText>
+      </AboutDivComponent>
+      <TechIcons>
+        <IconCss3 width={32} height={32} />
+        <IconHtml5 width={32} height={32} />
+        <IconTypescript width={26} height={26} />
+        <IconReact width={36} height={36} />
+      </TechIcons>
+    </AboutContainer>
   )
 }
--- src/components/About/styles.tsx
+++ src/components/About/styles.tsx
@@ -1,31 +1,43 @@
 import styled from 'styled-components'
 
+export const AboutContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
 export const AboutDivComponent = styled.div`
   display: flex;
+  gap: 2rem;
+  max-width: 75rem;
   align-items: center;
-  padding: 0.5rem;
-  margin: 1rem;
-  gap: 1rem;
   background-color: ${(props) => props.theme.darkGray};
   border-radius: 15px;
-  width: 70vw;
-  height: 7.5rem;
+  height: 16rem;
+  padding: 4rem;
 
   img {
-    width: 5rem;
-    height: 5rem;
-    border-radius: 99px;
-    box-shadow: -3.5px 2px ${(props) => props.theme.blue};
+    border-radius: 30px;
+    border: 2px solid ${(props) => props.theme.blue};
   }
 `
 
 export const AboutText = styled.div`
-  color: ${(props) => props.theme.latte};
-  max-width: 60vw;
-
-  h2 {
-    color: ${(props) => props.theme.latte};
-    text-shadow: 2px 2px 2px ${(props) => props.theme.blue};
-    margin-bottom: 0.3rem;
+  h1 {
+    margin-bottom: 1.2rem;
+    border-bottom: 2px solid ${(props) => props.theme.blue};
+    max-width: fit-content;
   }
+
+  p {
+    line-height: 2rem;
+  }
+`
+
+export const TechIcons = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-left: -3rem;
+  margin-top: 3rem;
+  gap: 1rem;
 `
